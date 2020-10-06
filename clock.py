10e06964bbfc1fbfa16bc01a88dc8727199d0d01
dariojcalo91 <dark_raven_d@hotmail.com>
2020-10-05 23:03:16 -0500

    generating auto tweet

--- clock.py
+++ clock.py
@@ -3,9 +3,9 @@ from app import postTweet
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=10)
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    print('This job is run every weekday at 10am.')
     postTweet()
 
 sched.start()
