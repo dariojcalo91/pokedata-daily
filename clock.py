8387317d91d534d2a4c7fc97a7015844347c79f6
Dario Calo <dark_raven_d@hotmail.com>
2022-05-04 16:52:17 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=9)
 def scheduled_job():
-    print('This job is run every day at 9am.')
+    print('This job will run every day at 9am.')
     postTweet()
 
 sched.start()
